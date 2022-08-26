f82b641e29cc2b716983ff7d9154b9132a5b93c6
DeepMy <441362952@qq.com>
2022-08-26 16:57:48 +0800

    aurora logist regression

--- diy_DT/LS_Regression_Tree.py
+++ diy_DT/LS_Regression_Tree.py
@@ -5,30 +5,46 @@ class LeastSquareRegressionTree:
         self.x = train_X
         self.y = y
         self.feature_count = train_X.shape[1]         # 特征总数
-        self.epsilon = epsilon                                  # 阈值
+        self.epsilon = epsilon                        # 阈值
         self.tree = None
 
-    def _fit(self, x, y, feature_count, epsilon):
+    def _buildTree(self, x, y, feature_count, epsilon):
         # 选择最优切分点变量 j 与 切分点 s
         (j, s, minval, c1, c2) = self._divede(x, y, feature_count)
+        # initial a tree
+        tree = {"feature": j, "value": x[s, j], "left": None, "right": None}
+        if minval < self.epsilon or len(y[np.where(x[:, j] <= x[s, j])]) <= 1:
+            tree["left"] = c1 # 左子树退出条件
+        else:
+            tree["left"] = self._buildTree(x[np.where(x[:, j] <= x[s, j])],
+                                     y[np.where(x[:, j] <= x[s, j])],
+                                     self.feature_count, epsilon)
+        if minval < self.epsilon or len(y[np.where(x[:, j] > x[s, j])]) <= 1:
+            tree["right"] = c2 # 右子树退出条件
+        else:
+            tree["right"] = self._buildTree(x[np.where(x[:, j] > x[s, j])],
+                                     y[np.where(x[:, j] > x[s, j])],
+                                     self.feature_count, epsilon)
+        return tree
+
 
     def fit(self):
-        self.tree = self._fit(self.x, self.y, self.feature_count, self.epsilon)
+        self.tree = self._buildTree(self.x, self.y, self.feature_count, self.epsilon)
 
     @staticmethod # 外部不需要创建实例，就可以调用该方法
-    def _divede(x, y, feature_count):
-        # 初始化 loss
+    def _divede(x: np.array, y: np.array, feature_count: int):
+        # 初始化 loss， shape = [特征个数，样本数量]
         cost = np.zeros((feature_count, len(x)))
         # eq 5.21
         for i in range(feature_count): # i为特征切割点
             for k in range(len(x)):
                 # k行i列 的特征值
                 value = x[k, i]
-                # 计算左子树
+                # 计算左子树 y的均值
                 y1 = y[np.where(x[:, i] <= value)]
                 c1 = np.mean(y1)
                 y1[:] = y1[:] - c1
-                # 计算右子树
+                # 计算右子树 y的均值
                 y2 = y[np.where(x[:, i] > value)]
                 c2 = np.mean(y2)
                 y2[:] = y2[:] - c2
@@ -36,21 +52,20 @@ class LeastSquareRegressionTree:
                 cost[i, k] = np.sum(y1**2) + np.sum(y2**2)
         # 选择最优损失误差点
         cost_idx = np.where(cost==np.min(cost))
-        # 选取最优特征，最优切分点
+        # 选取最优特征，最优样本切分点
         j, s = cost_idx[0][0], cost_idx[1][0]
         # 求两个区域的均值 c1， c2
         c1 = np.mean(y[np.where(x[:, j] <= x[s, j])])
         c2 = np.mean(y[np.where(x[:, j] > x[s, j])])
         return j, s, cost[cost_idx], c1, c2
 
-
 def main():
     train_X = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]).T
     y = np.array([4.50, 4.75, 4.91, 5.34, 5.80, 7.05, 7.90, 8.23, 8.70, 9.00])
 
     model_tree = LeastSquareRegressionTree(train_X, y, 0.2)
-
-
+    model_tree.fit()
+    print(model_tree.tree)
 
 if __name__=="__main__":
     main()
---
+++ diy_Logist_Regression/Max_Entropy_Model.py
@@ -0,0 +1,71 @@
+import math
+from copy import deepcopy
+
+class MaxEntropy:
+    def __init__(self, EPS=0.005):
+        self._samples = []
+        self._Y = set()     # 标签集合
+        self._numXY = {}    # key: (x, y)，value: 出现次数
+        self._N = 0         # 样本数
+        self._Ep_ = []      # 样本分布的特征期望值
+        self._xyID = {}     # key: 记录(x, y), value: id号
+        self._n = 0         # 特征键值 (x, y) 的个数
+        self._C = 0         # 最大特征数
+        self._IDxy = {}     # key: (x, y), value: id号
+        self._w = []
+        self._EPS = EPS     # 收敛条件
+        self._lastw = []    # 上一次 w参数值
+
+    def loadData(self, dataset):
+        self._samples = deepcopy(dataset)
+        for items in self._samples:
+            y = items[0]
+            X = items[1:]
+            self._Y.add(y)
+            for x in X:
+                if (x, y) in self._numXY:
+                    self._numXY[(x, y)] += 1
+                else:
+                    self._numXY[(x, y)] = 1
+
+        self._N = len(self._samples)
+        self._n = len(self._numXY)
+        self._C = max([len(sample)-1 for sample in self._samples])
+        self._w = [0] *self._n
+        self._lastw = self._w[:]
+
+        self._EP_ = [0]*self._n
+        for i, xy in enumerate(self._numXY): # 计算特征函数 fi 关于经验分布的期望
+            self._EP_[i] = self._numXY[xy] / self._N
+            self._xyID[xy] = i
+            self._IDxy[i] = xy
+
+    def _Zx(self, X):
+        """ 计算特征每个 Z(x)的值
+
+        :param X:
+        :return:
+        """
+        zx = 0
+        for y in self._Y:
+            ss = 0
+            for x in X:
+                if (x, y) in self._numXY:
+                    ss += self._w[self._xyID[(x, y)]]
+            zx += math.exp(ss)
+        return zx
+
+    def _model_pyx(self, y, X):
+        """ 计算每个 p(y|X)
+
+        :param y:
+        :param X:
+        :return:
+        """
+        zx = self._Zx(X)
+        ss = 0
+        for x in X:
+            if (x, y) in self._numXY:
+                ss += self._w[self._xyID[(x, y)]]
+        pyx = math.exp(ss) / zx
+        return pyx
