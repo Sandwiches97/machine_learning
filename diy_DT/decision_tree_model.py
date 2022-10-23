880dd2ba9e0df4139ed66755807bc999ae40731c
aurora <441362952@qq.com>
2022-10-23 22:55:27 +0800

    decision tree

--- diy_DT/decision_tree_model.py
+++ diy_DT/decision_tree_model.py
@@ -26,8 +26,8 @@ class DecisionNode():
         self.feature_i = feature_i
         self.threshold = threshold
         self.value = value
-        self.true_branch = true_branch
-        self.false_branch = false_branch
+        self.true_branch = true_branch          # 左子树
+        self.false_branch = false_branch        # 右子树
 
 # Super class of Regression Tree and Classification Tree
 class DecisionTree(object):
@@ -68,13 +68,13 @@ class DecisionTree(object):
         self.root = self._build_tree(X, y)
         self.loss = None
 
-    def _build_tree(self, X: np.ndarray, y, current_depth: int=0):
+    def _build_tree(self, X: np.ndarray, y: np.ndarray, current_depth: int=0):
         """
         递归方法
         :param X:
         :param y:
-        :param current_depth:
-        :return:
+        :param current_depth:   树的深度
+        :return:                a Decision Node
         """
         largest_impurity = 0
         best_criteria = None            # Feature index and Threshold
@@ -84,3 +84,61 @@ class DecisionTree(object):
         if len(np.shape(y))==1:
             y = np.expand_dims(y, axis=1)
         # Add y as last column of X
+        Xy = np.concatenate((X, y), axis=1)
+
+        n_samples, n_features = np.shape(X)
+
+        if n_samples >= self.min_samples_split and current_depth <= self.max_depth:
+            # Calculate the impurity for each features
+            for feature_i in range(n_features):
+                # All values of feature_i
+                feature_values = np.expand_dims(X[:, feature_i], axis=1)
+                unique_values = np.unique(feature_values)       # 类似 set 去重的作用
+
+                # Iterate through all unique values of feature column i
+                # and calculate the impurity
+                for threshold in unique_values:
+                    # Divide X and y depending on if the feature value of X
+                    # at index feature_i meets the threshold
+                    Xy1, Xy2 = divide_on_feature(Xy, feature_i, threshold)
+
+                    if len(Xy1) > 0 and len(Xy2) > 0:
+                        # Select the y-values of the two sets
+                        y1 = Xy1[:, n_features:]
+                        y2 = Xy2[:, n_features:]
+
+                        # Calculate impurity
+                        impurity = self._impurity_calculation(y, y1, y2)
+
+                        # If this threshold resulted in a higher information gain than
+                        # previously recorded save the threshold value and the feature index
+                        if impurity > largest_impurity:
+                            largest_impurity = impurity
+                            best_criteria = {"feature_i": feature_i,
+                                             "threshold": threshold}
+                            best_sets = {
+                                "leftX": Xy1[:, :n_features],  # X of left subtree
+                                "lefty": Xy1[:, n_features:],  # y of left subtree
+                                "rightX": Xy2[:, :n_features], # X of right subtree
+                                "righty": Xy2[:, n_features:]  # y of right subtree
+                            }
+        if largest_impurity > self.min_impurity:
+            # build subtrees for the right and left branches
+            true_branch = self._build_tree(best_sets["leftX"], best_sets["lefty"], current_depth+1)
+            false_branch = self._build_tree(best_sets["rightX"], best_sets["lefty"], current_depth+1)
+            return DecisionNode(feature_i=best_criteria["feature_i"],
+                                threshold=best_criteria["threshold"],
+                                true_branch=true_branch,
+                                false_branch=false_branch)
+
+        # We're at Leaf Node => determine value
+        leaf_value = self._leaf_value_calculation(y)
+        return DecisionNode(value=leaf_value)
+
+    def predict_value(self, X, tree):
+        """ Do a recursive search down the tree and
+        make a predicion of the data sample by the value of the leaf that we end up at
+        :param X:
+        :param tree:
+        :return:
+        """
